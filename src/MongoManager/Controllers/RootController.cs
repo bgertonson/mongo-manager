c519474190a823451f85ee05ed752102dc83abc9
bryan.gertonson <bryan@code-heroes.com>
2012-04-25 15:19:02 -0500

    initial site skeleton using twitter boostrap with empty homepage

---
+++ src/MongoManager/Controllers/RootController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MongoManager.Controllers
+{
+    public class RootController : Controller
+    {
+        //
+        // GET: /Root/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/MongoManager/Models/MongoManagerView.cs
@@ -0,0 +1,12 @@
+﻿namespace MongoManager.Models
+{
+    public class MongoManagerView
+    {
+        public string Server { get; set; }
+        public string Port { get; set; }
+
+        public string Database { get; set; }
+        public string Collection { get; set; }
+        public string DocumentId { get; set; }
+    }
+}
